88e5ee5815f059a61eb8033ef9f7d47d5767e6da
Gringo Suave <github@mgmiller.net>
2018-12-31 15:58:03 -0800

    refr: constants, regexes

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     scripts             = (proj_name,),
 
     classifiers         = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
