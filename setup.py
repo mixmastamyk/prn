780d3aa981c6eea4fe910cbb02efa8d784c8c801
Gringo Suave <mixmastamyk@bitbucket.org>
2025-10-17 13:14:50 -0700

    rm lic from clfers

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
